2749e20e795cbc3f319b81db6b6868fb92a6d007
Marco Angel <angel2367716@gmail.com>
2019-07-05 14:59:31 -0700

    Updated javascript file to capture user's input and store it on variable for later use. Problem: user input does not show up on with console log

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,23 @@
 $(document).ready(function(){
 
-let trainName = 0;
+//Captures button click 
+$('#add-train').on('click', function (event){
+    //prevents the page from re-loading
+    event.preventDefault();
+
+    // Captures the User Inputs and stores them into variables
+    const trainName = $("#trainName-input").val().trim();
+    const destination = $("#destination-input").val().trim();
+    const trainTime = $("#trainTime-input").val().trim();
+    const frequency = $("#frequency-input").val().trim();
+
+    // Console log each of the user inputs to confirm we are receiving them
+    console.log(trainName);
+    console.log(destination);
+    console.log(trainTime);
+    console.log(frequency);
+
+})
 
 
 
